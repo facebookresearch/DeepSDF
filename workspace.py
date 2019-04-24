9adc83b6854b4a6928da1092f862f8c941443497
Tanner Schmidt <tanner.schmidt@oculus.com>
2019-04-24 11:21:44 -0700

    workspace.py

---
+++ workspace.py
@@ -0,0 +1,5 @@
+model_params_subdir = "ModelParameters"
+optimizer_params_subdir = "OptimizerParameters"
+latent_codes_subdir = "LatentCodes"
+logs_filename = "Logs.pth"
+reconstructions_subdir = 'Reconstructions'
