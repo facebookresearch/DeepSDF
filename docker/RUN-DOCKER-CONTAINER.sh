54c95fef12a5917770c66cd72269c815cb9bc6fb
TMats <t.matsu.changmin@gmail.com>
2021-07-12 18:38:52 +0900

    Fix data directory

--- docker/RUN-DOCKER-CONTAINER.sh
+++ docker/RUN-DOCKER-CONTAINER.sh
@@ -5,5 +5,17 @@ xhost +
 
 ROOT=$(cd $(dirname $0); cd ../; pwd)
 
-nvidia-docker run --gpus all -it --net=host --ipc=host --name tmats_deepsdf -v /tmp/.X11-unix:/tmp/.X11-unix:rw  --privileged -v $ROOT:/workspace/DeepSDF -v $HOME/dataset:/workspace/dataset tmats/deepsdf:latest bash
+# Set DATASET_DIR if you want mount from other place than DeepSDF/data
+# The directory should be like $DATA_DIR/DeepSDF/data
+# The dataset directory will mount to /workspace/dataset/DeepSDF/data in the container
+if [ -z $DATASET_DIR]; then
+    DATASET_DIR=$(cd $ROOT; cd ../; pwd)
+fi
+echo "Data mounted from ${DATASET_DIR}"
+
+nvidia-docker run --gpus all -it --net=host --ipc=host --privileged -e DISPLAY=$DISPLAY --name tmats_deepsdf \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v $ROOT:/workspace/DeepSDF \
+    -v $DATASET_DIR:/workspace/dataset \
+    tmats/deepsdf:latest bash
 
